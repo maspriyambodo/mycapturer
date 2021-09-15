b6ae002a1ee8ea0863eefd2490e5296c71a78a46
maspriyambodo <maspriyambodo@gmail.com>
2021-09-15 08:22:52 +0700

    update git deploy

--- application/modules/Systems/controllers/Git_deploy.php
+++ application/modules/Systems/controllers/Git_deploy.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Git_deploy extends CI_Controller {
 
     public function index() {
-        shell_exec('cd /var/www/html/shandry/ && git reset –hard HEAD && git pull');
+        shell_exec('cd /var/www/html/shandry/ && git reset --hard HEAD && git pull');
         log_message('error', 'success pulling project');
     }
 

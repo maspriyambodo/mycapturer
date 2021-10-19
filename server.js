7d9902c88dfc4b6e13eb3f46b863e3748ace788c
bam <bedotpe@gmail.com>
2021-10-19 21:20:55 +0700

    update server.js

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
-var socket = require('socket.io');
+var cors = require('cors');
+var socket = require('socket.io')(server, {
+    wsEngine: require("eiows").Server,
+    transports: ["websocket", "polling"],
+    cors: {
+        origin: "https://sertifikasiku.mycapturer.com",
+        methods: ["GET", "POST"]
+    }
+});
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
@@ -9,12 +17,45 @@ server.listen(port, function () {
     console.log('Server listening at port %d', port);
 });
 
+
 io.on('connection', function (socket) {
 
     socket.on('new_message', function (data) {
-        io.sockets.emit('new_message', {
-            'msgtxt': data.msgtxt
-        });
+	if(!data.username){
+		return false;
+	} else{
+        	return io.sockets.emit('new_message', {
+			'username': data.username,
+			'fullname': data.fullname,
+			'key': data.key,
+			'msgtxt': data.msgtxt,
+			'avatar': data.avatar,
+			'name_role': data.role_name,
+			'id_user': data.id_user,
+			'id_role': data.id_role,
+			'chat_id': data.id_chat
+        	});
+	}
     });
+
+   socket.on('kick_user', function(data){
+	return io.sockets.emit(data.key, {
+		'uname': data.uname,
+		'chat_id': data.chat_id,
+		'user_id': data.user_id,
+		'msg': data.msg,
+		'category': 1
+	});
+   });
+
+  socket.on('warning_user', function(data){
+        return io.sockets.emit(data.key, {
+                'uname': data.uname,
+                'chat_id': data.chat_id,
+                'user_id': data.user_id,
+                'msg': data.msg,
+		'category': 2
+        });
+   });
 
 });
